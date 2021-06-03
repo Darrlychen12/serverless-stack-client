3814a110440457c14db24c909ea8d78b31733bc0
Darryl <darryl_chen@sfu.ca>
2021-06-03 14:47:41 -0700

    Building the react app

--- src/config.js
+++ src/config.js
@@ -13,6 +13,8 @@ const config = {
       APP_CLIENT_ID: "72tjus15gcgg81ign84kndevmk",
       IDENTITY_POOL_ID: "us-east-1:2229a9c0-de71-4e86-801c-5131e4065a8d",
     },
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_51IvTsMLC9TJeT2TYJPefFriZce7su0A9zuMhtufwgof1iEgSHeJ2Evop6jdZV1QhYdmjxwQBxelYSkCfoY6yasIQ00znN8JFN2",
   };
   
   export default config;
